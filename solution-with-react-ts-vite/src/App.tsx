52797ddf56d013276d6aab8abde0407ad091d422
Mathias Lund Ahrn <golvkaldt@gmail.com>
2023-02-14 20:59:06 +0100

    project structure with react-ts-vite-tailwind shell

---
+++ solution-with-react-ts-vite/src/App.tsx
@@ -0,0 +1,11 @@
+// import reactLogo from "./assets/react.svg";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-sky-300">
+      <h1 className="">HEEEY 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
